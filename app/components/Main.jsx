ae5c20a5c7132a67c24ab337ad7fa590fc8613a7
Min Soo Kim <gimmins@gmail.com>
2016-08-28 22:26:29 -0700

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Navigation = require('Navigation');
+
+var Main = (props) => {
+  return (
+    <div>
+      <Navigation/>
+      <div className="columns medium-6 large-4 small-centered">
+        { props.children }
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
